195b2ad5cb2f921d98cd3ecdb66fc0b1e0ffa53f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-12-26 16:51:44 +0100

    feat: add routing logic

--- nestjs-messages/src/messages/messages.controller.ts
+++ nestjs-messages/src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('/messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    console.log('List all messages');
+  }
+
+  @Post()
+  createMessage() {
+    console.log('Create a message');
+  }
+
+  @Get('/:id')
+  getMessage() {
+    console.log('Get a specific message');
+  }
+}
