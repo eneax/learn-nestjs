2fa4a178b71149e51aee30ea25bf0b42c83bc60d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-12-30 12:22:39 +0100

    feat: link MessagesController with MessagesService

--- nestjs-messages/src/messages/messages.controller.ts
+++ nestjs-messages/src/messages/messages.controller.ts
@@ -1,21 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // ! Controller is creating its own dependencies (Don't do this on real apps)
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    console.log('List all messages');
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log('Get message with id:', id);
+    return this.messagesService.findOne(id);
   }
 }
