77521f66c313605f68a22a949730ab5345bb1e57
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-01-01 15:04:03 +0100

    feat: create supplyPower method in PowerService

--- nestjs-computer/src/power/power.service.ts
+++ nestjs-computer/src/power/power.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  supplyPower(watts: number) {
+    console.log(`PowerService: supplying ${watts} watts of power.`);
+  }
+}
