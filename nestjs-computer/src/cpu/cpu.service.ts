00e319218eeff0c60f067a736c56b811207d2a44
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-01-02 21:21:47 +0100

    feat: create compute method in CpuService

--- nestjs-computer/src/cpu/cpu.service.ts
+++ nestjs-computer/src/cpu/cpu.service.ts
@@ -5,4 +5,10 @@ import { PowerService } from '../power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from Power Service.');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
