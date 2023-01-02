88bea7813a186f1f6c1dd7716ee851523e1da01c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-01-02 21:22:36 +0100

    feat: connect PowerModule to DiskModule

--- nestjs-computer/src/disk/disk.module.ts
+++ nestjs-computer/src/disk/disk.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { DiskService } from './disk.service';
+import { PowerModule } from 'src/power/power.module';
 
 @Module({
+  imports: [PowerModule],
   providers: [DiskService],
 })
 export class DiskModule {}
